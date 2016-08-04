01facb29dfd2b5d7dd033a1844eba962b57d7534
Gonzalo Marquez <gmarquez@mexnetwork.com>
2016-08-03 23:06:39 -0500

    learning directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Highlight } from './highlight.directive';
 
 @Component({
   moduleId: module.id,
   selector: 'dir-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [Highlight]
 })
 export class AppComponent {
   title = 'app works!';
+  private switch:boolean = true;
+  private items = [1,2,3,4,5];
+  private value:number = 10;
+
+  onSwitch() {
+    this.switch = !this.switch;
+  }
 }
